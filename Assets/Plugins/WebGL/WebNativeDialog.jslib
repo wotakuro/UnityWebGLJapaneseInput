7a71ab96b042aac1673203baec0bb0e4bd700d7c
wotakuro <wotakuro@gmail.com>
2016-05-31 19:37:14 +0900

    first commit

---
+++ Assets/Plugins/WebGL/WebNativeDialog.jslib
@@ -0,0 +1,10 @@
+﻿var WebNativeDialog = {
+   NativeDialogPrompt:function (title , defaultValue){
+     var result = window.prompt( Pointer_stringify(title) , Pointer_stringify(defaultValue) );
+     var buffer = _malloc(lengthBytesUTF8(result) + 1);
+     writeStringToMemory(result, buffer);
+     return buffer;
+   }
+};
+mergeInto( LibraryManager.library , WebNativeDialog );
+
---
+++ Assets/Scripts/WebNativeDialog.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class WebNativeDialog : MonoBehaviour {
+    [DllImport("__Internal")]
+    private static extern string NativeDialogPrompt(string title, string defaultValue);
+
+
+    public UnityEngine.UI.Text buttonText;
+
+	// Use this for initialization
+	void Start () {
+	}
+    public void OnClickButton()
+    {
+        string name = NativeDialogPrompt("名前を入力してください", buttonText.text);
+        Debug.Log("入力された名前：" + name);
+        buttonText.text = name;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
